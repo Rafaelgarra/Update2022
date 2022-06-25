8fa20adaddf6a3765e8518a673efc9e89e26b383
Rafael Quatis <rafaelgarra1@hotmail.com>
2022-06-25 20:37:42 -0300

    Update seller and custom service exception

---
+++ SalesWebMvc/Services/Exceptions/DbConcurrencyException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SalesWebMvc.Services.Exceptions
+{
+    public class DbConcurrencyException :ApplicationException
+    {
+        public DbConcurrencyException(string message) : base(message)
+        {
+
+        }
+    }
+}
